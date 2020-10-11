bbe652d459cb12530056e2969494899feca3da18
Ron Au <git@roncya.com>
2020-10-12 00:53:05 +1100

    Fix CORS

--- src/view/Input.svelte
+++ src/view/Input.svelte
@@ -36,7 +36,7 @@
 
       if (meta.status === 'OK') {
         // $audioStore.url = inputString;
-        $youtubeStore.url = 'https://cors-anywhere.herokuapp.com/' + getHighestAudioMP4URL(meta.formats);
+        $youtubeStore.url = 'https://corsno.herokuapp.com/' + getHighestAudioMP4URL(meta.formats);
         $youtubeStore.playable = true;
         $youtubeStore.title = meta.channelName;
       }
